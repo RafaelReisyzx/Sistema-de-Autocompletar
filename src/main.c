ed5d7aa5261b7d783cb495337b52e11b80b18733
RafaelReisyzx <102748011+RafaelReisyzx@users.noreply.github.com>
2023-09-22 22:03:20 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,46 +2,69 @@
 #include "System.h"
 
 int main() {
-    HashTable hash_table;
+    const char* input_files[] = {
+        "dataset/filosofia.txt",
+        "dataset/filosofia2.txt",
+        "dataset/globalizacao.txt",
+        "dataset/politica.txt",
+        "dataset/ti.txt",
+        "dataset/ti2.txt"
+    };
+
     int k = 20;
-    initializeHashTable(&hash_table);
-    loadStopWords(&hash_table, "dataset/stopwords.txt");
-    processFile(&hash_table, "dataset/filosofia.txt");
-
-    BinaryTreeNode* binaryTreeRoot = NULL;
-    AVLTreeNode* avlTreeRoot = NULL;
-    HuffmanNode* huffmanTreeRoot = NULL;
-    int treeSize = 0;
-
-    for (int i = 0; i < TABLE_SIZE; i++) {
-        Word* palavra_atual = hash_table.table[i];
-        while (palavra_atual != NULL) {
-            if (palavra_atual->frequency > 0) {
-                binaryTreeRoot = insertIntoBinaryTree(binaryTreeRoot, palavra_atual);
-                avlTreeRoot = insertIntoAVLTree(avlTreeRoot, palavra_atual);
-                huffmanTreeRoot = insertIntoHuffmanTree(huffmanTreeRoot, palavra_atual); // Adicione a palavra à árvore de Huffman
-                treeSize++;
-                if (treeSize > k) {
-                    binaryTreeRoot = deleteMinFromBinaryTree(binaryTreeRoot);
-                    avlTreeRoot = deleteMinFromAVLTree(avlTreeRoot);
-                    huffmanTreeRoot = deleteMinFromHuffmanTree(huffmanTreeRoot); // Remova a palavra da árvore de Huffman
-                    treeSize--;
-                }
-            }
-            palavra_atual = palavra_atual->next;
-        }
+    FILE* output_file = fopen("dataset/output.data", "w");
+    if (output_file == NULL) {
+        printf("Erro ao abrir o arquivo de saída.\n");
+        return 1;
     }
 
-    printf("Top %d palavras mais frequentes (Árvore Binária):\n", k);
-    printBinaryTreeInOrder(binaryTreeRoot);
+    for (int file_index = 0; file_index < 6; file_index++) {
+        HashTable hash_table;
+        initializeHashTable(&hash_table);
+        loadStopWords(&hash_table, "dataset/stopwords.data");
+        processFile(&hash_table, input_files[file_index]);
 
-    printf("\nTop %d palavras mais frequentes (Árvore AVL):\n", k);
-    printAVLTreeInOrder(avlTreeRoot);
+        BinaryTreeNode* binaryTreeRoot = NULL;
+        AVLTreeNode* avlTreeRoot = NULL;
+        HuffmanTreeNode* huffmanTreeRoot = NULL;
+        int treeSize = 0;
 
-    printf("\nTop %d palavras mais frequentes (Árvore de Huffman):\n", k);
-    printHuffmanTreeInOrder(huffmanTreeRoot); 
+        Word* frequent_words[k];
 
-    destroyHuffmanTree(huffmanTreeRoot); 
+        for (int i = 0; i < TABLE_SIZE; i++) {
+            Word* palavra_atual = hash_table.table[i];
+            while (palavra_atual != NULL) {
+                if (palavra_atual->frequency > 0) {
+                    frequent_words[treeSize] = palavra_atual;
+                    binaryTreeRoot = insertIntoBinaryTree(binaryTreeRoot, palavra_atual);
+                    avlTreeRoot = insertIntoAVLTree(avlTreeRoot, palavra_atual);
+                    treeSize++;
+                    if (treeSize > k) {
+                        binaryTreeRoot = deleteMinFromBinaryTree(binaryTreeRoot);
+                        avlTreeRoot = deleteMinFromAVLTree(avlTreeRoot);
+                        treeSize--;
+                    }
+                }
+                palavra_atual = palavra_atual->next;
+            }
+        }
+
+        huffmanTreeRoot = buildHuffmanTree(frequent_words, k);
+        char huffmanCode[MAX_WORD_LENGTH];
+        huffmanCode[0] = '\0';
+        fprintf(output_file, "Arquivo: %s\n", input_files[file_index]);
+        fprintf(output_file, "Top %d palavras mais frequentes (Árvore Binária):\n", k);
+        printBinaryTreeInOrderToFile(binaryTreeRoot, output_file);
+
+        fprintf(output_file, "\nTop %d palavras mais frequentes (Árvore AVL):\n", k);
+        printAVLTreeInOrderToFile(avlTreeRoot, output_file);
+
+        fprintf(output_file, "\nÁrvore de Huffman:\n");
+        generateHuffmanCodesToFile(huffmanTreeRoot, huffmanCode, 0, output_file);
+        fprintf(output_file, "\n==============================\n");
+    }
+
+    fclose(output_file);
 
     return 0;
 }
